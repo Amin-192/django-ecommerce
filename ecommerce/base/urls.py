47feac516f95c290898f574ce72dc4ce28766502
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-10-07 12:05:35 +0300

    added a working database and admin

--- ecommerce/base/urls.py
+++ ecommerce/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', views.home, name='home'), # Redirect to home page upon visiting root URL
     path('room/', views.room, name='room'), # Room page URL
-]
+    path('products/', views.products, name='products'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/base/views.py
+++ ecommerce/base/views.py
@@ -1,18 +1,22 @@
+# views.py
+
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
-from django.http import HttpResponse
+def home(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'base/home.html', context)
 
-
-
-
-
-def home(request): 
-    return render(request, 'base/home.html')
-   
-    
-
-
-    
 def room(request):
     return render(request, 'base/room.html')
+
+# This function name should be lowercase
+def products(request):
+    products = Product.objects.all()  # Fetch all products from the database
+    context = {
+        'products': products
+    }
+    return render(request, 'base/products.html', context)
