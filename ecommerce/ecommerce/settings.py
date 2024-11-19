bd7974f4d689e1bbe1978d39de55e1978bc7089f
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-11-19 12:52:08 +0300

    ready for deployment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1y(a2#=a3$2o=p%aoa77zz+aglfhxxgm!@9@a)(=fl%m9$r&na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-ecommerce-nine.vercel.app']
 
 
 # Application definition
